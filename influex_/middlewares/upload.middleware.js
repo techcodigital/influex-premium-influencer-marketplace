3c482748af96508ff2f80b964cdbd658462546ee
Ranu Patel <techco.digitalwork@gmail.com>
2026-05-02 14:12:47 +0530

    message

--- influex_/middlewares/upload.middleware.js
+++ influex_/middlewares/upload.middleware.js
@@ -1,36 +1,6 @@
-// import multer from "multer";
-// import multerS3 from "multer-s3";
-// import { s3 } from "../config/s3.js";
-
-// const upload = multer({
-//   storage: multerS3({
-//     s3,
-//     bucket: "influex-profile-images",
-//     contentType: multerS3.AUTO_CONTENT_TYPE,
-//     key: (req, file, cb) => {
-//       cb(null, `profiles/${Date.now()}-${file.originalname}`);
-//     },
-//   }),
-
-//   limits: {
-//     fileSize: 5 * 1024 * 1024,
-//   },
-
-//   fileFilter: (req, file, cb) => {
-//     if (file.mimetype.startsWith("image/")) {
-//       cb(null, true);
-//     } else {
-//       cb(new Error("Only images allowed"), false);
-//     }
-//   },
-// });
-
-// export default upload;
 import multer from "multer";
-import path from "path";
 import fs from "fs";
 
-// 🔥 ensure folder exist
 const uploadPath = "public/uploads/profiles";
 fs.mkdirSync(uploadPath, { recursive: true });
 
@@ -38,27 +8,20 @@ const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, uploadPath);
   },
-
   filename: (req, file, cb) => {
     const uniqueName = Date.now() + "-" + file.originalname;
     cb(null, uniqueName);
   },
 });
 
-const upload = multer({
+const uploadImage = multer({
   storage,
-
-  limits: {
-    fileSize: 5 * 1024 * 1024, // 5MB
-  },
-
+  limits: { fileSize: 5 * 1024 * 1024 },
   fileFilter: (req, file, cb) => {
-    if (file.mimetype.startsWith("image/")) {
-      cb(null, true);
-    } else {
-      cb(new Error("Only images allowed"), false);
-    }
+    file.mimetype.startsWith("image/")
+      ? cb(null, true)
+      : cb(new Error("Only images allowed"));
   },
 });
 
-export default upload;
+export default uploadImage;
