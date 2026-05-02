bc01a0fd3adfb60b519720c85944e9a43703873e
Ranu Patel <techco.digitalwork@gmail.com>
2026-05-02 14:37:07 +0530

    message

--- influex_/config/multer.js
+++ influex_/config/multer.js
@@ -17,8 +17,29 @@
 //   },
 // }); //console.log("AWS keys:", process.env.AWS_ACCESS_KEY,
 //  process.env.AWS_SECRET_KEY, process.env.AWS_REGION);
-import { S3Client } from "@aws-sdk/client-s3";
+import multer from "multer";
 
-export const s3 = new S3Client({
-  region: process.env.AWS_REGION,
+const storage = multer.diskStorage({
+  destination: "public/uploads/videos",
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + "-" + file.originalname);
+  },
 });
+
+const uploadVideo = multer({
+  storage,
+
+  limits: {
+    fileSize: 100 * 1024 * 1024,
+  },
+
+  fileFilter: (req, file, cb) => {
+    if (file.mimetype.startsWith("video/")) {
+      cb(null, true);
+    } else {
+      cb(new Error("Only video files allowed"), false);
+    }
+  },
+});
+
+export default uploadVideo;
