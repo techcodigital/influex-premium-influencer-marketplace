17d707f09de3b473d95361e51a6b94936d5e9d15
techcodigital <techcodigital@gmail.com>
2026-05-07 15:14:06 +0530

    Update video.middleware.js

--- influex_/middlewares/video.middleware.js
+++ influex_/middlewares/video.middleware.js
@@ -1,27 +1,27 @@
-import multer from "multer";
-import fs from "fs";
+// import multer from "multer";
+// import fs from "fs";
 
-const uploadPath = "public/uploads/videos";
-fs.mkdirSync(uploadPath, { recursive: true });
+// const uploadPath = "public/uploads/videos";
+// fs.mkdirSync(uploadPath, { recursive: true });
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, uploadPath);
-  },
-  filename: (req, file, cb) => {
-    const uniqueName = Date.now() + "-" + file.originalname;
-    cb(null, uniqueName);
-  },
-});
+// const storage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     cb(null, uploadPath);
+//   },
+//   filename: (req, file, cb) => {
+//     const uniqueName = Date.now() + "-" + file.originalname;
+//     cb(null, uniqueName);
+//   },
+// });
 
-const uploadVideo = multer({
-  storage,
-  limits: { fileSize: 100 * 1024 * 1024 },
-  fileFilter: (req, file, cb) => {
-    file.mimetype.startsWith("video/")
-      ? cb(null, true)
-      : cb(new Error("Only videos allowed"));
-  },
-});
+// const uploadVideo = multer({
+//   storage,
+//   limits: { fileSize: 100 * 1024 * 1024 },
+//   fileFilter: (req, file, cb) => {
+//     file.mimetype.startsWith("video/")
+//       ? cb(null, true)
+//       : cb(new Error("Only videos allowed"));
+//   },
+// });
 
-export default uploadVideo;
+// export default uploadVideo;
