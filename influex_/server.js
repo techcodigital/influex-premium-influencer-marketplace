df07358deb650bc017b00e5b42758d341d048df8
techcodigital <techcodigital@gmail.com>
2026-05-07 15:48:44 +0530

    Update server.js

--- influex_/server.js
+++ influex_/server.js
@@ -99,7 +99,7 @@ app.use("/api/invite",inviteRoutes)
 app.use("/api/payment",paymentsRoutes)
 app.use("/api/admin", adminRoutes)
 app.use("/api/dispute", disputeRoutes);
-app.use("/uploads", express.static("public/uploads"));
+app.use("/uploads", express.static("uploads"));
 // MongoDB connect + server start
 const PORT = process.env.PORT || 7000;
 
