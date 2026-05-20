d453abcb4694f94e33b69c90605729a7934cf75b
techcodigital <techcodigital@gmail.com>
2026-05-20 19:38:44 +0530

    Update compress.js

--- influex/utils/compress.js
+++ influex/utils/compress.js
@@ -1,29 +1,48 @@
 import ffmpeg from "fluent-ffmpeg";
+import ffmpegPath from "ffmpeg-static";
 import fs from "fs";
+
+ffmpeg.setFfmpegPath(ffmpegPath);
 
 export const compressVideo = (inputPath, outputPath) => {
   return new Promise((resolve, reject) => {
+
     ffmpeg(inputPath)
       .outputOptions([
         "-vf scale=720:-2",
         "-vcodec libx264",
-       "-crf 28",
-  "-maxrate 1000k",
-  "-bufsize 2000k",
+        "-crf 28",
+        "-maxrate 1000k",
+        "-bufsize 2000k",
         "-preset fast",
         "-movflags +faststart"
       ])
       .save(outputPath)
+
       .on("end", () => {
         console.log("✅ Video compressed");
-          // 🔥 YAHI ADD KARNA HAI
-      const originalSize = fs.statSync(inputPath).size / (1024 * 1024);
-const compressedSize = fs.statSync(outputPath).size / (1024 * 1024);
 
-console.log("📦 Original:", originalSize.toFixed(2), "MB");
-console.log("📦 Compressed:", compressedSize.toFixed(2), "MB");
+        const originalSize =
+          fs.statSync(inputPath).size / (1024 * 1024);
+
+        const compressedSize =
+          fs.statSync(outputPath).size / (1024 * 1024);
+
+        console.log(
+          "📦 Original:",
+          originalSize.toFixed(2),
+          "MB"
+        );
+
+        console.log(
+          "📦 Compressed:",
+          compressedSize.toFixed(2),
+          "MB"
+        );
+
         resolve();
       })
+
       .on("error", (err) => {
         console.log("❌ Compression error:", err);
         reject(err);
