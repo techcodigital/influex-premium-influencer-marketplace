4cdb0a514d39a68804c037cbd690a22f38e0740f
techcodigital <techcodigital@gmail.com>
2026-06-30 17:36:38 +0530

    Update auth.routes.js

--- influex/Routes/auth.routes.js
+++ influex/Routes/auth.routes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { signup, login,forgotPassword, resetPassword, verifyOtp,
-  resendOtp, } from "../controllers/auth.controller.js";
+  resendOtp, deleteAccount } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
@@ -10,5 +10,6 @@ router.post("/forgot-password", forgotPassword);
 router.post("/reset-password", resetPassword);
 router.post("/verify-otp", verifyOtp);
 router.post("/resend-otp", resendOtp);
+router.delete("/delete-account", deleteAccount);
 
 export default router;
